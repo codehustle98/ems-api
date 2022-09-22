70961a44781e041a11d0a1380a36639283ed6ff9
codehustle98 <codehustle98@gmail.com>
2022-09-22 21:28:05 +0530

    initial commit

---
+++ commons/src/main/java/com/ems/commons/constants/ApplicationConstants.java
@@ -0,0 +1,6 @@
+package com.ems.commons.constants;
+
+public class ApplicationConstants {
+
+    public static final String DATE_FORMAT = "yyyy-MM-dd";
+}
---
+++ core/src/main/java/com/ems/core/entity/Employee.java
@@ -0,0 +1,37 @@
+package com.ems.core.entity;
+
+import com.ems.commons.constants.ApplicationConstants;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "employee")
+@Data
+public class Employee implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "emp_id")
+    private Long empId;
+
+    @Column(name = "emp_name",nullable = false,insertable = true,updatable = true,length = 25)
+    private String empName;
+
+    @Column(name = "emp_email_id",nullable = false,insertable = true,updatable = true,unique = true)
+    private String empEmailId;
+
+    @Column(name = "emp_phone_no",,nullable = false,insertable = true,updatable = true,unique = true)
+    private String empPhoneNo;
+
+    @Column(name = "emp_dob",nullable = false,updatable = true)
+    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = ApplicationConstants.DATE_FORMAT)
+    private LocalDate empDob;
+
+    @Column(name = "emp_join_date",nullable = false,updatable = true)
+    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = ApplicationConstants.DATE_FORMAT)
+    private LocalDate empJoinDate;
+}
