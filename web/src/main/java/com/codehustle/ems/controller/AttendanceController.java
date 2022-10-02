fc1e3306c059421a27a6a64c89503e28ff25737f
arun1996ap <30997425+arun1996ap@users.noreply.github.com>
2022-10-02 18:38:37 +0530

    Add Regularize Method

--- web/src/main/java/com/codehustle/ems/controller/AttendanceController.java
+++ web/src/main/java/com/codehustle/ems/controller/AttendanceController.java
@@ -3,9 +3,13 @@ package com.codehustle.ems.controller;
 import com.codehustle.ems.exceptions.ServiceException;
 import com.codehustle.ems.model.Attendance;
 import com.codehustle.ems.service.AttendanceService;
+import jdk.vm.ci.meta.Local;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping(value = "/attendance")
@@ -24,5 +28,14 @@ public class AttendanceController {
     @ResponseStatus(HttpStatus.OK)
     public void addCheckOutAttendance(@PathVariable(name = "attendanceId")Long attendanceId) throws ServiceException {
         attendanceService.addCheckoutAttendance(attendanceId);
+    }
+
+    @PutMapping(value = "/regularize")
+    public void regularizeAttendance(@RequestParam(name="checkInTime",required = true)LocalDateTime checkinTime,
+                                     @RequestParam(name="checkOutTime",required = true) LocalDateTime checkOutTime,
+                                     @RequestParam(name="attendanceId",required = false) Long attandanceId,
+                                     @RequestParam(name="employeeId",required = false)Long empId,
+                                     @RequestParam(name = "attendanceDate",required = false)LocalDate attendanceDate) throws ServiceException {
+            attendanceService.regularizeAttendance(checkinTime,checkOutTime,attandanceId,empId,attendanceDate);
     }
 }
--- web/src/main/java/com/codehustle/ems/service/AttendanceService.java
+++ web/src/main/java/com/codehustle/ems/service/AttendanceService.java
@@ -2,9 +2,13 @@ package com.codehustle.ems.service;
 
 import com.codehustle.ems.exceptions.ServiceException;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public interface AttendanceService {
 
     void addAttendance(Long empId);
 
     void addCheckoutAttendance(Long attendanceId) throws ServiceException;
+    void regularizeAttendance(LocalDateTime checkinTime, LocalDateTime checkOutTime, Long attandanceId, Long empId, LocalDate attendanceDate) throws ServiceException;
 }
