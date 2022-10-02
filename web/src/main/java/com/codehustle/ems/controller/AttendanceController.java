41fe22d8191a1927d8da5516174e7b91deb4d8cc
codehustle98 <codehustle98@gmail.com>
2022-10-02 13:59:03 +0530

    added attendance apis

--- web/src/main/java/com/codehustle/ems/controller/AttendanceController.java
+++ web/src/main/java/com/codehustle/ems/controller/AttendanceController.java
@@ -1,5 +1,6 @@
 package com.codehustle.ems.controller;
 
+import com.codehustle.ems.exceptions.ServiceException;
 import com.codehustle.ems.model.Attendance;
 import com.codehustle.ems.service.AttendanceService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,9 +14,15 @@ public class AttendanceController {
     @Autowired
     private AttendanceService attendanceService;
 
-    @PostMapping
+    @PostMapping(value = "{empId}")
     @ResponseStatus(HttpStatus.CREATED)
-    public void addAttendance(@RequestBody Attendance attendance){
-        attendanceService.addAttendance(attendance);
+    public void addAttendance(@PathVariable(name = "empId")Long empId){
+        attendanceService.addAttendance(empId);
+    }
+
+    @PutMapping(value = "{attendanceId}")
+    @ResponseStatus(HttpStatus.OK)
+    public void addCheckOutAttendance(@PathVariable(name = "attendanceId")Long attendanceId) throws ServiceException {
+        attendanceService.addCheckoutAttendance(attendanceId);
     }
 }
